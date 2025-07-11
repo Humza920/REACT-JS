fdd89455d27dd5b1ee4781c6ec7e4bbc5ee62ffb
Humza920 <humza.rehman.779@gmail.com>
2025-07-11 05:18:06 +0500

    PASSSWORD GENERATOR

--- PRACTICE/password-generator-ts/src/App.tsx
+++ PRACTICE/password-generator-ts/src/App.tsx
@@ -2,14 +2,30 @@ import { useState , useCallback } from "react";
 
 const App = () => {
   // Generic type annotation use hua hai ts ka
-  const [input , setinput] =  useState<string>('')
+  const [pass , setpass] =  useState<string>('')
   const [length , setlength] = useState<string>("8")
   const [character , setcharacter] = useState<boolean>(false)
   const [number , setnumber] = useState<boolean>(false)
 
 const generatePassword = useCallback(()=>{
-  let a
+  let arr: string[] =[ "a","b","c","d","e","f","g","h","i","j","k","l","m",
+  "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  if (character) {
+    arr.push( "!", "@", "#", "$", "&", "*", "_", ".", "<", ">", "/", "?")
+  }
+    if (number) {
+    arr.push("0","1","2","3","4","5","6","7","8","9")
+  }
+  console.log(arr);
+  let password:string = ''
+  for (let i = 0; i <= Number(length); i++) {
+  let char = Math.floor(Math.random()*arr.length)
+  password = arr.charAt()
+  
+  }
 },[length , character , number])
+
+
 
   return (
     <>
