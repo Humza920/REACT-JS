b175eb752069e6ac385b749966229c5a27b767fb
Humza920 <humza.rehman.779@gmail.com>
2025-07-11 07:42:19 +0500

    PASSWORD GENERATOR DONE

--- PRACTICE/password-generator-ts/src/App.tsx
+++ PRACTICE/password-generator-ts/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState , useCallback } from "react";
+import { useState , useCallback , useEffect ,useRef } from "react";
 
 const App = () => {
   // Generic type annotation use hua hai ts ka
@@ -6,26 +6,39 @@ const App = () => {
   const [length , setlength] = useState<string>("8")
   const [character , setcharacter] = useState<boolean>(false)
   const [number , setnumber] = useState<boolean>(false)
-
-const generatePassword = useCallback(()=>{
-  let arr: string[] =[ "a","b","c","d","e","f","g","h","i","j","k","l","m",
-  "n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  if (character) {
-    arr.push( "!", "@", "#", "$", "&", "*", "_", ".", "<", ">", "/", "?")
-  }
-    if (number) {
-    arr.push("0","1","2","3","4","5","6","7","8","9")
-  }
-  console.log(arr);
-  let password:string = ''
-  for (let i = 0; i <= Number(length); i++) {
-  let char = Math.floor(Math.random()*arr.length)
-  password = arr.charAt()
+  const passRef = useRef<HTMLInputElement>(null)
   
+  const copytoclipboard = useCallback(()=>{
+    console.log(passRef.current?.select());
+    
+      window.navigator.clipboard.writeText(pass)
+    },[pass])
+    
+    const generatePassword = useCallback(()=>{
+      let password:string = ''
+      let arr: string[] =[ "a","b","c","d","e","f","g","h","i","j","k","l","m",
+        "n","o","p","q","r","s","t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L","M",
+        "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        if (character) {
+          arr.push( "!", "@", "#", "$", "&", "*", "_", "-", "/", "?")
+        }
+        if (number) {
+          arr.push("0","1","2","3","4","5","6","7","8","9")
+        }
+        console.log(arr);
+  for (let i = 1; i <= Number(length); i++) {
+  let char = Math.floor(Math.random()*arr.length)
+  password += arr[char]
   }
-},[length , character , number])
+  console.log(password);
+  setpass(password)
+  
+},[length , character , number ])
 
-
+useEffect(
+  generatePassword
+  
+  ,[length , character , number ,])
 
   return (
     <>
@@ -39,9 +52,13 @@ const generatePassword = useCallback(()=>{
             className="px-4 py-3 text-lg w-full outline-none bg-gray-100 text-black placeholder-gray-600"
             type="text"
             placeholder="Generate your Password"
+            readOnly
+            ref={passRef}
+            value={pass}
           />
           <button
             type="button"
+            onClick={copytoclipboard}
             className="bg-blue-600 hover:bg-blue-700 active:scale-95 transition-all text-white font-semibold px-5 cursor-pointer"
           >
             Copy
