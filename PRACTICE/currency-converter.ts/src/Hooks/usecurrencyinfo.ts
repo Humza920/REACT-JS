af13ec8ed71584c116a37e80a93f4fd96892c4f8
Humza920 <humza.rehman.779@gmail.com>
2025-07-14 01:52:57 +0500

    CUSTOM HOOK CREATED

--- PRACTICE/currency-converter.ts/src/Hooks/usecurrencyinfo.ts
+++ PRACTICE/currency-converter.ts/src/Hooks/usecurrencyinfo.ts
@@ -1,5 +1,24 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 
-function usecurrencyinfo (currency:string) {
-    
+// CUSTOM TYPE ALLIAS USING UTILITY TYPE
+type currencyData = {
+  date: string;
+  usd: Record<string, number>;
+};
+
+function usecurrencyinfo(currency: string): currencyData {
+  const [data, setdata] = useState<currencyData>({
+    date: "",
+    usd: {},
+  });
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res: Response) => res.json())
+      .then((res: currencyData) => setdata(data));
+  }, [currency]);
+
+  return data
 }
