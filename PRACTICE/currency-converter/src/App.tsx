7441a5d9918bd99b061859fa142772d718b6f3ba
Humza920 <humza.rehman.779@gmail.com>
2025-07-22 05:33:59 +0500

    NEW

---
+++ PRACTICE/currency-converter/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState, useCallback } from "react";
+import useCurrencyinfo from "./Hook/useCurrencyinfo";
+
+const App = () => {
+  useCurrencyinfo("usd");
+  
+
+
+  return <>hello</>;
+};
+
+export default App;
---
+++ PRACTICE/currency-converter/src/Hook/useCurrencyinfo.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+type Data = {
+  date: string;
+  [key: string]: Record<string, number> | string;
+};
+
+function useCurrencyinfo(currency: string): Record<string, number> {
+  const [data, setdata] = useState<Record<string, number>>({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res : Response) => res.json())
+      .then((res : Data) => setdata(res[currency] as Record<string , number>));
+  }, [currency]);
+  console.log(data);
+  
+  return data;
+}
+
+export default useCurrencyinfo
