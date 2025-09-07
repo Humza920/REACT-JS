96853955d8ababbe19665f244801e19da8178ab9
Humza920 <humza.rehman.779@gmail.com>
2025-09-07 16:14:35 +0500

    THEME TOOGLER DONE

--- PRACTICE/theme-switcher2/src/App.jsx
+++ PRACTICE/theme-switcher2/src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
-import { ThemeProvider } from './contexts/themecontext'
-
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/themecontext";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
+  const [theme , settheme] = useState("light")
+  const darktheme = ()=>{
+    settheme("dark")
+  }
+  const lighttheme = ()=>{
+    settheme("light")
+  }
+
+useEffect(() => {
+  document.querySelector("html").classList.remove("dark" , "light")
+  document.querySelector("html").classList.add(theme)
+}, [theme])
+
 
   return (
-<ThemeProvider>
-  
-</ThemeProvider>
-  )
+    <ThemeProvider value={{ theme, darktheme, lighttheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
